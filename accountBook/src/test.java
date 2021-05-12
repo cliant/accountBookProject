5361a0ae2dfdb52162565d0b874cefd852a98b34
st0421 <81230496+st0421@users.noreply.github.com>
2021-05-12 13:48:28 +0900

    Update test.java

--- accountBook/src/test.java
+++ accountBook/src/test.java
@@ -2,6 +2,7 @@ import java.io.Console;
 
 public class test {
 	public static void main(String[] args) {
+		System.out.println();
 		Console a = System.console();
 		String sample = a.readLine();
 		System.out.println(sample);
